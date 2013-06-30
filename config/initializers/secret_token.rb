2c1c75a96e1bdea012cc451656f85563465dc404
buxiangshuo <moshuohua@gmail.com>
2013-06-30 12:19:43 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Witkey::Application.config.secret_key_base = '0e431ae4fe6d62e6f7bcb72d38bab4f479894f35a5111563b001941a0055b8e28fbba10223cf2ad343f0b1eed5a885c5d132d6b9439d4058c47f5412b0d4a61a'
